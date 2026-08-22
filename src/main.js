85d30ad17c768027143853e54257cfcf2abb4cf3
RockerHDlol <cre04@icloud.com>
2026-08-22 12:17:35 +0000

    pre load

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ import { OutputPass } from "three/addons/postprocessing/OutputPass.js";
 import gsap from "gsap";
 
 // Set to true to preload Instagram and YouTube content during startup.
-const PRELOAD_REELS = false;
+const PRELOAD_REELS = true;
 
 /**
  * ✅ Interaction lock:
